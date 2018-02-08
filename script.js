3aa63354223fcbfab1531df16b1c746bdda60e81
bbeck3@lsu.edu <garlic99>
2018-02-08 12:08:59 -0600

    adding picures of assignments

---
+++ script.js
@@ -0,0 +1,9 @@
+let projectId= 1
+let projectTitle = "story Map"
+let thumbnail = true
+let projectTitles = ["Story Map", "Book Assignment 1: Web Story Map"]
+
+console.log(projectId)
+console.log(projectTitle)
+console.log(thumbnail)
+console.log(projectTitles)
