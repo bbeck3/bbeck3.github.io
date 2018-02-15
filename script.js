46cfbf1fbe13f28353fee9ceb0c8363e5484b84d
bbeck3@lsu.edu <garlic99>
2018-02-15 11:51:40 -0600

    adding array in java

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ let projectTitle = "story Map"
 let thumbnail = true
 let projectTitles = ["Story Map", "Book Assignment 1: Web Story Map"]
 
-console.log(projectId)
-console.log(projectTitle)
-console.log(thumbnail)
-console.log(projectTitles)
+let projectarray= [
+  1,
+  '',
+  '',
+  '',
+  true,
+  ['earthquakes', 'usgs']
+]
+
+let projectObject = [
+  id: 1,
+  url: ''
+  title: ''
+  description: ''
+  hasThumbnail: true
+  keywords: ['earthquakes', 'usgs']
+]
+
+console.log(projectarray)
