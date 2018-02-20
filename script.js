4003c1916c201bb2760cf63fa2013e6de75bd2fd
bbeck3@lsu.edu <garlic99>
2018-02-20 10:42:57 -0600

    code assignment 4

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ let projectTitles = ["Story Map", "Book Assignment 1: Web Story Map"]
 
 let projectarray= [
   1,
-  '',
-  '',
-  '',
+  'http://arcg.is/2DMo71c',
+  ' Book Assignment 1: Web Map Creation',
+  'View a small tour of Philadelphia',
   true,
   ['earthquakes', 'usgs']
 ]
 
 let projectObject = [
   id: 1,
-  url: ''
-  title: ''
-  description: ''
+  url: 'http://arcg.is/2DMo71c'
+  title: 'Book Assignment 1: Web Map Creation'
+  description: 'View a small tour of Philadelphia'
   hasThumbnail: true
   keywords: ['earthquakes', 'usgs']
 ]
