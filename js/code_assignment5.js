94e76f476edafef8e9f01a0a9c399c9d091e5cb4
bbeck3@lsu.edu <garlic99>
2018-02-27 11:15:56 -0600

    code5

--- js/code_assignment5.js
+++ js/code_assignment5.js
@@ -1,20 +1,24 @@
-let project1 = {
+let projectData = []
+
+projectData[0] = {
   id: 1,
   title: ' Book Assignment 1: Web Map Creation',
   hasThumbnail: true
   url: 'http://arcg.is/2DMo71c'
   description: 'View a small tour of Philadelphia'
 }
-let project2 = {
+projectData[1] = {
   id: 2,
   title: 'Book Assignment 2',
   hasThumbnail: true
   url:'https://arcg.is/0445am'
   description:'Click here to see a web application that is a world Map of significant earthquakes in the past 30 days'
 }
-let projects = [
-  project1,
-  project2
-]
+
 for (let i = 0; i < projects.length; i++) {
-  
+  if(projectData[i].thumb === true){
+    console.log('images/ss-project' + projectData[i].id + '.PNG')
+  } else {
+    console.log('images/no-preview.png')
+  }
+}
