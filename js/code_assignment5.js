f90ad27b6783940072425905b9f4d91e955adffa
bbeck3@lsu.edu <garlic99>
2018-02-27 11:25:38 -0600

    code5 edits

--- js/code_assignment5.js
+++ js/code_assignment5.js
@@ -3,19 +3,19 @@ let projectData = []
 projectData[0] = {
   id: 1,
   title: ' Book Assignment 1: Web Map Creation',
-  hasThumbnail: true
-  url: 'http://arcg.is/2DMo71c'
-  description: 'View a small tour of Philadelphia'
+  thumb: true,
+  url: 'http://arcg.is/2DMo71c',
+  desc: 'View a small tour of Philadelphia'
 }
 projectData[1] = {
   id: 2,
   title: 'Book Assignment 2',
-  hasThumbnail: true
-  url:'https://arcg.is/0445am'
-  description:'Click here to see a web application that is a world Map of significant earthquakes in the past 30 days'
+  thumb: true,
+  url:'https://arcg.is/0445am',
+  desc:'Click here to see a web application that is a world Map of significant earthquakes in the past 30 days'
 }
 
-for (let i = 0; i < projects.length; i++) {
+for (let i = 0; i < projectData.length; i++) {
   if(projectData[i].thumb === true){
     console.log('images/ss-project' + projectData[i].id + '.PNG')
   } else {
