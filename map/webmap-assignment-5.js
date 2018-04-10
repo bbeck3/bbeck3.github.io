751c3a6f94a2948b2c6dd020ad0966a031af18f8
bbeck3@lsu.edu <garlic99>
2018-04-10 11:28:30 -0500

    webmap5

---
+++ map/webmap-assignment-5.js
@@ -0,0 +1,50 @@
+let map5 = L.map('map5id').setView([38,-98], 4)
+let greenBasemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
+let greenBasemap = L.tileLayer(greenBasemapUrl)
+greenBasemap.addTo(map5)
+
+let whiteBasemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+let whiteBasemap = L.tileLayer(whiteBasemapUrl)
+
+let satelliteUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+let satelliteBasemap = L.tileLayer(satelliteUrl)
+
+let myBasemaps = {
+  'Light basemap': whiteBasemap,
+  'Dark basemap': greenBasemap,
+  'Satellite': satelliteBasemap
+}
+
+L.control.layers(myBasemaps).addTo(map5)
+
+let myControlOptions = {
+  collapsed: false
+}
+
+geoJSONstyle = function (state){
+  let popmales = state.properties.MALES
+  let popfemales = state.properties.FEMALES
+ let stateColor = 'red'
+ if ( popmales < popfemales ) { stateColor = 'blue' }
+ let geoJSONstyle = {
+   color: stateColor,
+   weight: 2,
+   fillOpacity: .3
+}
+return geoJSONstyle
+}
+
+function createPopup (state, stateLayer){
+  let name = state.properties.STATE_NAME
+    let age = state.properties.MED_AGE
+    stateLayer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+  }
+
+
+geoJSONOptions = {
+  style: geoJSONstyle,
+  onEachFeature: createPopup
+}
+
+
+L.geoJSON(StateData, geoJSONOptions).addTo(map5)
